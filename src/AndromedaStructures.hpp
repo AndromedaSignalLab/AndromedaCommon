8a85abefe22388a323be213138e3facc745d955c
Volkan Orhan <contactme@volkanorhan.com>
2026-05-11 00:28:30 +0300

    Move application version management system from ModPlugPlayer

--- src/AndromedaStructures.hpp
+++ src/AndromedaStructures.hpp
@@ -82,5 +82,90 @@ namespace AndromedaSignalLab {
     {
         FMT_U8, FMT_S8, FMT_U16_LE, FMT_U16_BE, FMT_U16_NE, FMT_S16_LE, FMT_S16_BE, FMT_S16_NE
     };
+
+
+    struct Version {
+        unsigned int major = 0;
+        unsigned int minor = 0;
+        unsigned int release = 0;
+        unsigned int patch = 0;
+        Stage stage = Stage::StableRelease;
+        unsigned int stageRevision = 0;
+
+        inline std::string prettyString();
+        inline std::string technicalString();
+    };
+}
+
+inline std::string AndromedaSignalLab::Version::technicalString() {
+    std::ostringstream version;
+    version << major << "." << minor;
+
+    if (release != 0 || patch != 0) {
+        version << "." << release;
+    }
+
+    if (patch != 0) {
+        version << "." << patch;
+    }
+
+    switch (stage) {
+        case Stage::PreAlpha:
+            version << "-pre-alpha";
+            break;
+        case Stage::Alpha:
+            version << "-alpha";
+            break;
+        case Stage::Beta:
+            version << "-beta";
+            break;
+        case Stage::ReleaseCandidate:
+            version << "-rc";
+            break;
+        case Stage::StableRelease:
+            break;
+        default:
+            break;
+    }
+
+    if (stage != Stage::StableRelease && stageRevision != 0)
+        version << "." << stageRevision;
+
+    return version.str();
+}
+
+inline std::string AndromedaSignalLab::Version::prettyString() {
+    std::ostringstream version;
+    version << major << "." << minor;
+
+    if (release != 0 || patch != 0) {
+        version << "." << release;
+    }
+
+    if (patch != 0) {
+        version << "." << patch;
+    }
+
+    switch (stage) {
+        case Stage::PreAlpha:
+            version << " Pre-Alpha";
+            break;
+        case Stage::Alpha:
+            version << " Alpha";
+            break;
+        case Stage::Beta:
+            version << " Beta";
+            break;
+        case Stage::ReleaseCandidate:
+            version << " Release Candidate";
+            break;
+        default:
+            break;
+    }
+
+    if (stage != Stage::StableRelease && stageRevision != 0)
+        version << " " << stageRevision;
+
+    return version.str();
 }
 
